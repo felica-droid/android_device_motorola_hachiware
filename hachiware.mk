bcfe5c8cd00538c75c7b97d4be9f07d549936c7d
lahaina <nattolecats@gmail.com>
2024-07-02 01:13:31 +0900

    hachiware: nfc: Pull libnfc-nxp.conf

--- hachiware.mk
+++ hachiware.mk
@@ -77,6 +77,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-mtp-SN100.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-mtp-SN100.conf
 
 # Perf
