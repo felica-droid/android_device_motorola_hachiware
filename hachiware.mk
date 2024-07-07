c6459a02a357e2ead23c69d6b4adea6944d881ee
lahaina <34679325+nattolecats@users.noreply.github.com>
2024-07-07 14:26:24 +0900

    hachiware: Drop unneeded AIDL NFC HAL

--- hachiware.mk
+++ hachiware.mk
@@ -58,7 +58,6 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.nxp \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     libchrome.vendor \
