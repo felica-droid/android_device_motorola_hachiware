c51f5fc94f85ed67ea6ea3887286ad0692b1cbc3
lahaina <nattolecats@gmail.com>
2024-09-13 21:23:45 +0900

    penang: Rising up

--- lineage_penang.mk
+++ lineage_penang.mk
@@ -11,8 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from penang device
 $(call inherit-product, device/motorola/hachiware/device-penang.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Rising stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Rising
+RISING_MAINTAINER := lahaina
+RISING_CHIPSET := Snapdragon 480 Plus 5G
+RISING_PACKAGE_TYPE := GAPPS
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_penang
@@ -28,6 +40,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="penang_g-user 14 U1TPS34M.29-83-1 295da release-keys"
 
 BUILD_FINGERPRINT := motorola/penang_g/penang:14/U1TPS34M.29-83-1/295da:user/release-keys
-
-# Gapps
-TARGET_USES_MINI_GAPPS := true
